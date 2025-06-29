8a039fd408cd6c21eeecc79f5a5e2e596de3a5cf
RaghadAbusamor <raghadmohammad253@gmail.com>
2025-06-30 00:46:25 +0200

    Update Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "sky5tech",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Sky5Tech - IT Company",
 };
 
 export default function RootLayout({
@@ -19,8 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* <link rel="icon" href="/jsm-logo.png" sizes="any" /> */}
-        <link rel="icon" href="/Systemizer.png" sizes="any" />
+         
+        {/* <link rel="icon" href="/Sky5tech.png" sizes="any" /> */}
+        /* <link rel="icon" href="/jsm-logo.png" sizes="any" />  */
       </head>
       <body className={inter.className}>
         <ThemeProvider
