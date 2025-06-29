237fb4f3180631b891b8b17ddc94ae0973a2bf7b
ayatallan <ayat.online99@hotmail.com>
2025-06-30 01:21:57 +0300

    edite the logo in browser

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        {/* <link rel="icon" href="/jsm-logo.png" sizes="any" /> */}
+        <link rel="icon" href="/Systemizer.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
